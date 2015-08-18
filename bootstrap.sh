7396ebf860164f73969dbf6cb9e2a6b78bc6cb3e
River MacLeod <river@mm0hai.net>
2015-08-18 14:04:21 +0100

    Update vagrant and ansible install.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,10 @@
 sudo apt-get install git python-pip python-jinja2 python-yaml openssh-server aptitude
 
 # Vagrant in ubuntu is too old
-wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.2_x86_64.deb
-sudo dpkg -i vagrant_1.6.2_x86_64.deb
+wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.4_x86_64.deb
+sudo dpkg -i vagrant_1.7.4_x86_64.deb
 
 git clone https://github.com/ansible/ansible.git
 cd ansible
+git submodule update --init --recursive
 sudo make install
